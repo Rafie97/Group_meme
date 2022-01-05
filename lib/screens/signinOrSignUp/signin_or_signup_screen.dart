4f18dfc4c211e35dc0527df0f1391ffa047ee056
rafa <rajosh80@gmail.com>
2022-01-04 22:09:27 -0600

    Add ability to sign in with google

--- lib/screens/signinOrSignUp/signin_or_signup_screen.dart
+++ lib/screens/signinOrSignUp/signin_or_signup_screen.dart
@@ -3,10 +3,43 @@ import 'package:meme_messenger/constants.dart';
 import 'package:meme_messenger/screens/chats/chats_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 class SigninOrSignupScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void NavToChats() {
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ChatsScreen(),
+        ),
+      );
+    }
+
+    Future<UserCredential> signInWithGoogle() async {
+      if (kIsWeb) {
+        GoogleAuthProvider googleProvider = GoogleAuthProvider();
+        googleProvider
+            .addScope('https://www.googleapis.com/auth/contacts.readonly');
+        googleProvider.setCustomParameters({'login_hint': 'user@example.com'});
+        NavToChats();
+        return await FirebaseAuth.instance.signInWithPopup(googleProvider);
+      } else {
+        final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+        final GoogleSignInAuthentication? googleAuth =
+            await googleUser?.authentication;
+
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth?.accessToken,
+          idToken: googleAuth?.idToken,
+        );
+        NavToChats();
+        return await FirebaseAuth.instance.signInWithCredential(credential);
+      }
+    }
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -22,22 +55,16 @@ class SigninOrSignupScreen extends StatelessWidget {
               ),
               Spacer(),
               PrimaryButton(
+                color: Theme.of(context).colorScheme.secondary,
+                text: "Sign In With Google",
+                press: signInWithGoogle,
+              ),
+              SizedBox(height: kDefaultPadding * 1.5),
+              PrimaryButton(
                   text: "Sign In Anonymously",
                   press: () {
                     FirebaseAuth.instance.signInAnonymously();
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => ChatsScreen(),
-                      ),
-                    );
                   }),
-              SizedBox(height: kDefaultPadding * 1.5),
-              PrimaryButton(
-                color: Theme.of(context).colorScheme.secondary,
-                text: "Sign Up",
-                press: () {},
-              ),
               Spacer(flex: 2),
             ],
           ),
